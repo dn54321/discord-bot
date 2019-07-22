45010a39d44d44bbd876de6059892851755ac24b
Daniel Minh Pham <daniel_m_pham@hotmail.com>
2019-07-22 23:05:40 +1000

    fixed a bug where everyone was silence

--- Swear_Filter.py
+++ Swear_Filter.py
@@ -10,18 +10,18 @@ class Swear_Filter(commands.Cog):
         self.remove = Remove_Role()
     @commands.Cog.listener()
     async def on_message(self, message):
-        print("Hello")
         predict_percent = predict([message.content])[0]
         channel = message.channel
-        await self.role.prep(self.bot, Payload(user_id=message.author.id), None,None, "+『si̸̲̬l̢͓̲en̺̹͞c͖͈͘ȩ』")
         if predict_percent == 1:
             await channel.send("The message has been deemed inappropriate. You have been silence for 5 minutes.")
             await message.delete()
+            await self.role.prep(self.bot, Payload(user_id=message.author.id), None,None, "+『si̸̲̬l̢͓̲en̺̹͞c͖͈͘ȩ』")
             await asyncio.sleep(60*3)
             await self.remove.prep(self.bot, Payload(user_id=message.author.id), None,None, "+『si̸̲̬l̢͓̲en̺̹͞c͖͈͘ȩ』")
 
         elif predict([message.content]) > 0.7:
             await channel.send("The message has been flagged for scanning. You have been silence for 3 minutes.")
             await message.delete()
+            await self.role.prep(self.bot, Payload(user_id=message.author.id), None,None, "+『si̸̲̬l̢͓̲en̺̹͞c͖͈͘ȩ』")
             await asyncio.sleep(60*1)
             await self.remove.prep(self.bot, Payload(user_id=message.author.id), None,None, "+『si̸̲̬l̢͓̲en̺̹͞c͖͈͘ȩ』")
